d29870ae95d00115c3bfdcbc9fcdc6798162c1f3
aripbudiman <muhamadarifbudiman22@gmail.com>
2023-11-19 17:00:47 +0700

    membuat database

---
+++ database/migrations/2023_11_19_094432_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->foreignId('author_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+            $table->date('published');
+            $table->longText('content');
+            $table->boolean('draft')->default(false);
+            $table->text('thumbnail')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
